1975b8b2109a3ac37121ccb5baf8bc22635dbc35
Mamtilda <mamtathind195@gmail.com>
2023-03-17 22:47:43 +0000

    updated pom file and added TestNG config

--- src/main/java/SeleniumTest.java
+++ src/main/java/SeleniumTest.java
@@ -1,72 +1,81 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-
 public class SeleniumTest {
 
-    WebDriver driver;
+    private WebDriver driver;
 
-    public void amazonLaunch() {
+    @BeforeMethod
+    public void setUp() {
         System.setProperty("webdriver.gecko.driver", "C:/Users/Mamta Thind/Downloads/geckodriver-v0.32.2-win32/geckodriver.exe");
         FirefoxOptions options = new FirefoxOptions();
         options.setBinary("C:/Program Files/Mozilla Firefox/firefox.exe");
         driver = new FirefoxDriver(options);
-        driver.get("https://www.amazon.co.uk");
     }
-    public void acceptCookies() {
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void testAmazon() {
+        amazonLaunch();
+        acceptCookies();
+        amazonSearch();
+        amazonSelectItem();
+        amazonAddToCart();
+        amazonRemoveFromCart();
+        amazonHomePage();
+    }
+
+    private void amazonLaunch() {
+        driver.get("https://www.amazon.co.uk/");
+    }
+
+    private void acceptCookies() {
         WebElement acceptButton = driver.findElement(By.id("sp-cc-accept"));
         acceptButton.click();
     }
 
-    public void amazonSearch() {
-        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("speakers");
-        driver.findElement(By.id("nav-search-submit-button")).click();
+    private void amazonSearch() {
+        WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
+        searchBox.sendKeys("speakers");
+        searchBox.submit();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[data-asin] h2 a")));
     }
 
-    public void amazonSelectItem() {
+    private void amazonSelectItem() {
         driver.findElement(By.cssSelector("[data-asin] h2 a")).click();
     }
-    public void amazonAddToCart() {
-        driver.findElement(By.id("add-to-cart-button")).click();
 
+    private void amazonAddToCart() {
+        driver.findElement(By.id("add-to-cart-button")).click();
     }
 
-    public void amazonRemoveFromCart() {
+    private void amazonRemoveFromCart() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement basketButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[aria-labelledby='attach-sidesheet-view-cart-button-announce']")));
         basketButton.click();
 
-
         WebElement deleteButton = driver.findElement(By.xpath("//input[@value='Delete']"));
         deleteButton.click();
-
-        // Close the browser
-        driver.quit();
-
-
-
-    }
-    public static void main(String [] args) {
-        SeleniumTest test = new SeleniumTest();
-
-        test.amazonLaunch();
-        test.acceptCookies();
-        test.amazonSearch();
-        test.amazonSelectItem();
-        test.amazonAddToCart();
-        test.amazonRemoveFromCart();
-
     }
 
-
-
+    private void amazonHomePage() {
+        driver.findElement(By.id("nav-logo-sprites")).click();
+    }
 }
